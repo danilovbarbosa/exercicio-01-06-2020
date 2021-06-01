2e2f7abcd33f485adbaf37e1841b695d3e960c3a
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-01 17:39:11 -0300

    feat(app/models): adicionado métodos get para classe Produto.

--- app/models.py
+++ app/models.py
@@ -1,14 +1,25 @@
-
 class Produto:
     
     def __init__(self, nome, descricao, valor):
-        self.nome = nome
-        self.descricao = descricao
-        self.valor = valor
+        self.__nome = nome
+        self.__descricao = descricao
+        self.__valor = valor
         
+    @property
+    def get_nome(self):
+        return self.__nome
+    
+    @property
+    def get_descricao(self):
+        return self.__descricao
+
+    @property
+    def get_valor(self):
+        return self.__valor
+
 
 class Categoria:
     
     def __init__(self, nome, descricao):
-        self.nome = nome
-        self.descricao = descricao
+        self.__nome = nome
+        self.__descricao = descricao
