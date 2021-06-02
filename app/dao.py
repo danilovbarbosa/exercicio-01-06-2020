194f32c704ea9e879a98489a7d1476dff002a9e3
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-02 09:46:15 -0300

    test(test/test_produto): foi implementada test_deve_criar_um_arquivo em TestProduto.

--- app/dao.py
+++ app/dao.py
@@ -8,7 +8,7 @@ from .util import File
 
 
 class CategoriaDAO:
-    
+
     def __init__(self, nome_do_arquivo):
         self.nome_do_arquivo = nome_do_arquivo
         File.create(nome_do_arquivo)
@@ -16,23 +16,22 @@ class CategoriaDAO:
     def create(self, categoria: Categoria) -> NoReturn:
         File.write(self.nome_do_arquivo, categoria)
 
-
     def read(self) -> List[Categoria]:
         lista_categorias_str = File.read(self.nome_do_arquivo).splitlines()
         return [Categoria(i[0], i[1]) for i in lista_categorias_str]
 
     def delete(self):
         pass
-    
+
     def update(self):
-        pass    
-    
-    
+        pass
+
+
 class ProdutoDAO:
-    
+
     def __init__(self, nome_do_arquivo):
         self.nome_do_arquivo = nome_do_arquivo
-    File.create(nome_do_arquivo)
+        File.create(nome_do_arquivo)
 
     def create(self, produto: Produto) -> NoReturn:
         File.write(self.nome_do_arquivo, produto)
@@ -42,7 +41,6 @@ class ProdutoDAO:
 
     def delete(self):
         pass
-    
+
     def update(self):
-        pass    
-    
+        pass
