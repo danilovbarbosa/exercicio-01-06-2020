55d7649c95b7d1a6c0c9468528dce78410ee05ef
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-02 10:04:08 -0300

    test(test/test_categoria): foi implementada test_deve_ler_o_arquivo_com_categorias em TestCategoria.

--- app/test/test_categoria.py
+++ app/test/test_categoria.py
@@ -5,7 +5,7 @@ from app.dao import CategoriaDAO
 
 class TestCategoria:
     
-    def test_deve_criar_um_arquivo(self):
+    def test_deve_criar_um_arquivo_com_uma_categoria(self):
         categoria = Categoria("nome teste", "nome descricao")
         
         nome_do_arquivo = os.getcwd() + "/" + "test_categoria.txt"
@@ -16,3 +16,13 @@ class TestCategoria:
         isExist = os.path.exists(nome_do_arquivo)
         
         assert isExist is True, f"O valor deveria ser True, porém foi {isExist}"
+        
+
+    def test_deve_ler_o_arquivo_com_categorias(self):       
+        nome_do_arquivo = os.getcwd() + "/" + "test_categoria.txt"
+        
+        categoriaDAO = CategoriaDAO(nome_do_arquivo)
+
+        list_categorias = categoriaDAO.read()
+        
+        assert isinstance(list_categorias, list), f"O valor deveria ser [], porém foi {list_categorias}"
