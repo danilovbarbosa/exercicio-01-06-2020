0e60d283f9dd20b01386dc347810a9e385fafc1a
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-02 10:05:50 -0300

    refactor(test/test_produto): foi renomeada test_deve_criar_um_arquivo_com_uma_produto em TestProduto.

--- app/test/test_produto.py
+++ app/test/test_produto.py
@@ -5,7 +5,7 @@ from app.dao import ProdutoDAO
 
 class TestProduto:
     
-    def test_deve_criar_um_arquivo(self):
+    def test_deve_criar_um_arquivo_com_uma_produto(self):
         categoria = Categoria("nome teste", "nome descricao")
 
         produto = Produto("nome teste", "nome descricao", 33, categoria.__str__())
@@ -18,3 +18,12 @@ class TestProduto:
         isExist = os.path.exists(nome_do_arquivo)
         
         assert isExist is True, f"O valor deveria ser True, porém foi {isExist}"
+    
+    def test_deve_ler_o_arquivo_com_categorias(self):       
+        nome_do_arquivo = os.getcwd() + "/" + "test_categoria.txt"
+        
+        categoriaDAO = CategoriaDAO(nome_do_arquivo)
+
+        list_categorias = categoriaDAO.read()
+        
+        assert isinstance(list_categorias, list), f"O valor deveria ser [], porém foi {list_categorias}"
